534ea28368b8c128bb5e389a3c98ec01671acadb
Amir Heyward <amirheyward2@gmail.com>
2026-06-10 22:31:16 -0400

    Implemnted card component using API

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import "./App.css";
+import Card from "./components/Card.tsx"
 import { getCurrentTrack } from "./api";
 import { useState } from "react";
 
 function App() {
-  const [content, setContent] = useState("");
+  const [content, setContent] = useState(<></>);
 
   return (
     <div>
       <button
         onClick={async () => {
           const data = await getCurrentTrack();
-          setContent(JSON.stringify(data));
+          setContent(<Card album={data.album} song={data.song} artist={data.artist} playing={data.playing} />);
         }}
       >click</button>
       <div>{content}</div>
